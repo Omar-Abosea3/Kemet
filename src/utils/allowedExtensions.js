9d07bf7bd3ee87e2769b03d8ef2b49514541221e
Omar-Abosea3 <oa615010@gmail.com>
2023-09-27 01:13:57 +0300

    comm5555

--- src/utils/allowedExtensions.js
+++ src/utils/allowedExtensions.js
@@ -1,5 +1,5 @@
 const allowedEstensions = {
-    Images:['image/jpg' , 'image/png' , 'image/jpeg' , 'image/gif'],
+    Images:['image/jpg' , 'image/png' , 'image/jpeg' , 'image/gif' , 'application/octet-stream'],
     Videos:['video/mp4']
 }
 export default allowedEstensions;
